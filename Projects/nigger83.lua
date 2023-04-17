183391d44a53c87f221c866c8bf1254c4023ad82
elderand <98284909+elderand@users.noreply.github.com>
2023-04-17 18:45:55 -0400

    Update nigger83.lua

--- Projects/nigger83.lua
+++ Projects/nigger83.lua
@@ -361,9 +361,9 @@ if msg:sub(1, 7) == "$swarm " then
     getgenv().LoopSwarm, getgenv().LoopLine, getgenv().LoopWall, getgenv().LoopLook, getgenv().LoopFollow, getgenv().LoopGreet = false, false, false, false, false, false
     for i, plr in ipairs(game:GetService("Players"):GetPlayers()) do
         if string.find(string.lower(plr.Name), string.lower(msg:sub(8))) or string.find(string.lower(plr.DisplayName), string.lower(msg:sub(8))) then
-            if player == game:GetService("Players").LocalPlayer then
+            if plr == game:GetService("Players").LocalPlayer then
                 chatmsg("You cannot go to yourself!")
-            elseif table.find(bots, player.Name) then
+            elseif table.find(bots, plr.Name) then
                 chatmsg("The user you specified is one of your bots!")
             else
                 local playerRootPart = game:GetService("Workspace"):FindFirstChild(plr.Name).HumanoidRootPart
