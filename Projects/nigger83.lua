f3500b3abed72f01c2596162c27b4d26779b0d9f
elderand <98284909+elderand@users.noreply.github.com>
2023-04-17 18:39:58 -0400

    Update nigger83.lua

--- Projects/nigger83.lua
+++ Projects/nigger83.lua
@@ -361,6 +361,11 @@ end
             getgenv().LoopSwarm, getgenv().LoopLine, getgenv().LoopWall, getgenv().LoopLook, getgenv().LoopFollow, getgenv().LoopGreet = false, false, false, false, false, false
             for i, plr in ipairs(game:GetService("Players"):GetPlayers()) do
                 if string.find(string.lower(plr.Name), string.lower(msg:sub(8))) or string.find(string.lower(plr.DisplayName), string.lower(msg:sub(8))) then
+                    if player == game:GetService("Players").LocalPlayer then
+                    chatmsg("The user you specified is one of your bots!")   
+                    elseif table.find(bots, player.Name) then
+                    chatmsg("The user you specified is one of your bots!")
+                    else
                     local playerRootPart = game:GetService("Workspace"):FindFirstChild(plr.Name).HumanoidRootPart
                     if table.find(bots, game:GetService("Players").LocalPlayer.Name) then
                         getgenv().LoopSwarm = true
@@ -372,13 +377,16 @@ end
                 end
             end
         end
+                end
         
         if msg:sub(1, 8) == "$lookat " then
             getgenv().LoopSwarm, getgenv().LoopLine, getgenv().LoopWall, getgenv().LoopLook, getgenv().LoopFollow, getgenv().LoopGreet = false, false, false, false, false, false
             for _, plr in ipairs(game:GetService("Players"):GetPlayers()) do
                 if string.lower(plr.Name):sub(1, string.len(msg:sub(9))) == string.lower(msg:sub(9)) or
                    string.lower(plr.DisplayName):sub(1, string.len(msg:sub(9))) == string.lower(msg:sub(9)) then
-                    if table.find(bots, player.Name) then
+                    if player == game:GetService("Players").LocalPlayer then
+                    chatmsg("The user you specified is one of your bots!")   
+                    elseif table.find(bots, player.Name) then
                     chatmsg("The user you specified is one of your bots!")
                     else
                     local playerRootPart = game:GetService("Workspace"):FindFirstChild(plr.Name).HumanoidRootPart
